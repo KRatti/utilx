e3b43724687bb3d2544a81c60df1acebc77d406b
KRatti <kyle.ratti@outlook.com>
2013-12-12 12:09:56 -0500

    Added support for mutli line queries

--- src/sql/RunFile.java
+++ src/sql/RunFile.java
@@ -7,10 +7,12 @@ package utilx.sql;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /** Run SQL statements in a file! */
@@ -24,34 +26,60 @@ public class RunFile
 	*/
 	public static void run(String strFile, Connection objConnection)
 	{
-		try
+		File objFile = new File(strFile);
+
+		if(objFile.exists())
 		{
-			File objFile = new File(strFile);
-
-			if(objFile.exists())
+			for(String strQuery : RunFile.getQueries(objFile))
 			{
-				Scanner objScanner = new Scanner(objFile);
-
-				while(objScanner.hasNextLine())
+				try
 				{
-					try
-					{
-						objConnection.createStatement().executeQuery(objScanner.nextLine());
-					}
-					catch(SQLException e)
-					{
-						e.printStackTrace();
-					}
+					objConnection.createStatement().executeQuery(strQuery);
+				}
+				catch(SQLException e)
+				{
+					e.printStackTrace();
 				}
 			}
-			else
-			{
-				System.out.println("File not found!");
-			}
 		}
-		catch(IOException e)
+		else
 		{
-			System.out.println("Unable to read file (?!)");
+			System.out.println("File not found!");
 		}
+	}
+
+	/**
+	* Gets an array of all queries in the file
+	*
+	* @return An <code>ArrayList</code> of queries
+	*/
+	private static ArrayList<String> getQueries(File objFile)
+	{
+		try
+		{
+			Scanner objScanner = new Scanner(objFile);
+			objScanner.useDelimiter(";");
+
+			ArrayList<String> objQueries = new ArrayList<String>();
+
+			while(objScanner.hasNext())
+			{
+				String strQuery = (objScanner.next() + ";").replace("\n", "").replace("\r", "");
+
+				if(strQuery.length() > 1)
+				{
+					System.out.println(strQuery);
+					objQueries.add(strQuery);
+				}
+			}
+
+			return objQueries;
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+
+		return new ArrayList<String>();
 	}
 }
