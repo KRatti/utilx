e88ff5d9d3edeb0a4e8808a9070982b6ec079387
KRatti <kyle.ratti@outlook.com>
2013-12-22 00:02:31 -0500

    build_jar.bat now copies to shared ownCloud bin folder

--- build_jar.bat
+++ build_jar.bat
@@ -3,4 +3,5 @@ javac -Xlint:unchecked src/*.java src/sql/*.java
 if not exist build/utilx mkdir build/utilx
 xcopy /Q /E /Y src\*.class build\utilx\
 jar cvf utilx.jar -C build/ .
+if exist D:\ownCloud\Documents\School\Java\bin\ xcopy /Q /Y utilx.jar D:\ownCloud\Documents\School\Java\bin\
 @pause
