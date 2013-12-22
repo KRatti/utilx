82895db3dee6dd43feae44e5b8c91902693d0bcd
KRatti <kyle.ratti@outlook.com>
2013-12-22 10:51:37 -0500

    Added InvalidUsageException

---
+++ src/InvalidUsageException.java
@@ -0,0 +1,20 @@
+/**
+* @author Kyle Ratti (PC18)
+* @version 1.0, 12/22/13
+*/
+
+package utilx;
+
+/** An exception for the CLICommand class */
+public class InvalidUsageException extends RuntimeException
+{
+	/**
+	* Creates a new InvalidUsageException with the specified error message
+	*
+	* @param strMsg The error message
+	*/
+	public InvalidUsageException(String strMsg)
+	{
+		super(strMsg);
+	}
+}
