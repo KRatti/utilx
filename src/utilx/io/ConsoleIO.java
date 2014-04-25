49109175de61e4fcb41c4f9ab1a495b012342af3
KRatti <kyle.ratti@outlook.com>
2014-04-25 19:51:24 -0400

    Added Prompt.getChar

---
+++ src/utilx/io/ConsoleIO.java
@@ -0,0 +1,108 @@
+package utilx.io;
+
+import java.io.Console;
+import java.io.IOError;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+/**
+ * @author Kyle
+ * @version 1.0, 4/25/2014
+ */
+public class ConsoleIO {
+	/**
+	 * The Scanner object to use
+	 */
+	private static final Scanner inputScanner = new Scanner(System.in);
+	/**
+	 * The Console object to use
+	 */
+	private static final Console inputConsole = System.console();
+	/**
+	 * Should user input be shown?
+	 */
+	private static boolean showInput = true;
+
+	/**
+	 * Print an empty line
+	 */
+	public static void println() {
+		System.out.println();
+	}
+
+	/**
+	 * Print an Object
+	 * @param objData The Object to print
+	 */
+	public static void println(Object objData) {
+		System.out.println(objData);
+	}
+
+	/**
+	 * Print an empty line to the error stream
+	 */
+	public static void errorln() {
+		System.err.println();
+	}
+
+	/**
+	 * Print an Object to the error stream
+	 * @param objData The Object to print
+	 */
+	public static void errorln(Object objData) {
+		System.err.println(objData);
+	}
+
+	public static void setShowInput(boolean bShow) {
+		ConsoleIO.showInput = bShow;
+	}
+
+	public static boolean getShowInput() {
+		return ConsoleIO.showInput;
+	}
+
+	/*
+	* TODO
+	*   - getChar
+	*   - getDouble
+	*   - getInt
+	 */
+
+	public static String getString() throws NoSuchElementException {
+		String strInput = null;
+
+		if(ConsoleIO.getShowInput()) {
+			try {
+				strInput = ConsoleIO.inputScanner.nextLine();
+			} catch (IllegalStateException e) {
+				ConsoleIO.errorln("Error getting String: " + e.getMessage());
+			}
+		}
+		else {
+			try {
+				strInput = new String(ConsoleIO.inputConsole.readPassword());
+
+				if(strInput.length() <= 0)
+					throw new NoSuchElementException();
+			} catch (IOError e) {
+				ConsoleIO.errorln("Error getting hidden String: " + e.getMessage());
+			}
+		}
+
+		return strInput;
+	}
+
+	public static Character getChar() throws NoSuchElementException, IllegalStateException, IOError {
+		String strInput;
+
+		if(ConsoleIO.getShowInput())
+			strInput = ConsoleIO.inputScanner.next();
+		else
+			strInput = new String(ConsoleIO.inputConsole.readPassword());
+
+		if(strInput.length() <= 0)
+			throw new NoSuchElementException();
+
+		return strInput.charAt(0);
+	}
+}
---
+++ src/utilx/io/Prompt.java
@@ -0,0 +1,20 @@
+package utilx.io;
+
+/**
+ * @author Kyle
+ * @version 1.0, 4/25/2014
+ */
+public class Prompt {
+	/**
+	 * The version of Prompt
+	 */
+	private static final double VERSION = 4.0;
+
+	/**
+	 * Get the version of Prompt
+	 * @return The version of Prompt
+	 */
+	public double getVersion() {
+		return Prompt.VERSION;
+	}
+}
