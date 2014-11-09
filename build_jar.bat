2680767e4f625b4e1e9e586d78da5148804fa278
KRatti <kyle.ratti@outlook.com>
2014-11-08 21:47:10 -0500

    Removed old build files

--- build_jar.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-javac -Xlint:unchecked src/*.java src/sql/*.java src/cli/*.java
-if not exist build/utilx mkdir build/utilx
-xcopy /Q /E /Y src\*.class build\utilx\
-jar cvf utilx.jar -C build/ .
-if exist D:\ownCloud\Documents\School\Java\bin\ xcopy /Q /Y utilx.jar D:\ownCloud\Documents\School\Java\bin\
-@pause
